136242ee2327f7ef2d44f705cab964b4eeff73fa
yair taboch <taboch.yair@gmail.com>
2019-03-10 18:23:17 +0200

    added second semester's תנודות lab

--- lab_first_year/second_semester/vibrations/matlab/first_matlab_fit.m
+++ lab_first_year/second_semester/vibrations/matlab/first_matlab_fit.m
@@ -1,5 +1,6 @@
 
 function f = first_messure (x,a)
+% a(1) = v_0, a(2) = \emoga, a(3) = \phi, a(4)= error in messurment
 b = sin(a(2).*x + a(3));
 f = a(1).*b +a(4);
 % i'ts ok that it won't fit.there is toooooooo many errors.
--- lab_first_year/second_semester/vibrations/matlab/rcl_circuit.m
+++ lab_first_year/second_semester/vibrations/matlab/rcl_circuit.m
@@ -1,3 +1,3 @@
 function f = rc_circuit (x,a)
-% Y =v, x = t, a3 = \omega, a5 = \tau,
-f = a(1) + a(2).*cos(a(3) * x + a(4)) .*exp(-x./a(5));
+% Y =v, x = t, a3 = \omega, a5 = \tau, a(6) = error in messurment
+f = a(1) + a(2).*cos(a(3) * x + a(4)) .*exp(-x./a(5) + a(6));
