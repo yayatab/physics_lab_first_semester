f1ee14c4605668482079df516141caeb9679f2e4
yair taboch <taboch.yair@gmail.com>
2019-03-10 10:00:05 +0200

    oopsies

---
+++ general_tools/statistics.py
@@ -0,0 +1,15 @@
+import numpy
+
+
+# def mean_value(*args):
+#     """args = ilst of tuples(x, p(x)"""
+#     l = list(args)
+#     return sum([x(0) * x(1) for x in l])
+
+
+def expected_value(array)
+    return numpy.mean(array)
+
+
+def expected_value_weights(array, weights):
+    return numpy.average(array, weights)
