b772865b28756e799a8e28daf3e11121a6ef98b6
yair taboch <taboch.yair@gmail.com>
2019-03-25 11:42:26 +0200

    added find_constant

--- constants.py
+++ constants.py
@@ -1,7 +1,33 @@
 from math import pi
+from scipy import constants as con
+from scipy.constants import find as fnd
 
 AVOGADRO_NUMBER = 6.022140857e23
 G_THEO_CM = 981.3
 G_THEO_CM_D = 10
 G_THEO = G_THEO_CM / 100
 G_THEO_D = G_THEO_CM_D / 100
+
+
+def find_constant(name=None) -> dict:
+    """
+    returns a dictionary of the constant name and it's value:
+    for example:
+    !!! ALL THE CONSTANTS ARE IN SI UNITS !!!
+    :param name:
+    :type name: str
+    :return: dictionary of name and value
+    :rtype: dict
+    """
+    res = fnd(name)
+    if res is None:
+        print("constant not found")
+    else:
+        ret = {}
+        for key in res:
+            ret.update({key: [con.value(key), con.unit(key)]})
+    return ret
+
+
+def get_units(something: str):
+    return con.unit(something)
