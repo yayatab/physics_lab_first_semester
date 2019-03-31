84ff5a617d87fbe1a5cda00ef8824f619655f6af
yair taboch <taboch.yair@gmail.com>
2019-03-31 09:16:17 +0300

    added round for lab

--- general_tools/general_math.py
+++ general_tools/general_math.py
@@ -1,5 +1,5 @@
 from math import factorial as factor
-from math import exp, sqrt, atan, atan2
+from math import exp, sqrt, atan, atan2, log10, floor
 import math
 
 
@@ -17,3 +17,21 @@ def binomial_cooeficiant(n, k):
 
 def calc_asin_on_lists(A, B):
     return [math.asin(x / y) for x, y in zip(A, B)]
+
+
+def round_to(x, d):
+    """
+    round a number to d digits.
+    examples:
+    in                     | out
+    ------------------------------
+    round_to_2(123.456, 0) | 100.0
+    round_to_2(123.456, 1) | 120.0
+    round_to_2(123.456, 6) | 123.456
+    round_to_2(123.456, 5) | 123.456
+    round_to_2(123.456, 4) | 123.46
+    :param x: the number
+    :param d: number of digits to round to
+    :return: the rounded number
+    """
+    return round(x, d - int(floor(log10(abs(x)))))
